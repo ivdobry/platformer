540f3588748d94977316c41b1db57fcabfd353c2
ivdobry <ivan@rzilient.club>
2024-11-23 11:03:04 +0100

    make a rectangle move

--- main.lua
+++ main.lua
@@ -1,3 +1,30 @@
+function love.load()
+    rect = {}
+    rect.x = 100
+    rect.y = 100
+    rect.width = 70
+    rect.height = 90
+    rect.speed = 200
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("d") then
+        rect.x = rect.x + rect.speed * dt
+    end
+
+    if love.keyboard.isDown("a") then
+        rect.x = rect.x - rect.speed * dt
+    end
+
+    if love.keyboard.isDown("w") then
+        rect.y = rect.y - rect.speed * dt
+    end
+
+    if love.keyboard.isDown("s") then
+        rect.y = rect.y + rect.speed * dt
+    end
+end
+
 function love.draw()
-    love.graphics.rectangle("fill", 100, 200, 50, 80)
+    love.graphics.rectangle("line", rect.x, rect.y, rect.height, rect.width)
 end
