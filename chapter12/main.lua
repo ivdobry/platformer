d03107b0371581f00b24619e23b6ea8f95c13c25
ivdobry <ivan@rzilient.club>
2024-11-25 14:38:33 +0100

    images

---
+++ chapter12/main.lua
@@ -0,0 +1,15 @@
+function love.load()
+    myImage = love.graphics.newImage("sheep.png")
+    width = myImage:getWidth()
+    height = myImage:getHeight()
+end
+
+function love.draw()
+    love.graphics.setColor(255 / 255, 200 / 255, 40 / 255, 127 / 255)
+    love.graphics.setColor(1, 0.78, 0.15, 0.5)
+    -- Or ...
+    love.graphics.draw(myImage, 100, 100)
+    -- Not passing an argument for alpha automatically sets it to 1 again.
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(myImage, 200, 100)
+end
